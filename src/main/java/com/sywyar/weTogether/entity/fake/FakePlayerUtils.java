236ef27588e094b5ab559e47e858f8a95f52b1c5
Sywyar <Sywyari@163.com>
2024-06-08 17:27:47 +0800

    Update FakePlayerUtils.java

--- src/main/java/com/sywyar/weTogether/entity/fake/FakePlayerUtils.java
+++ src/main/java/com/sywyar/weTogether/entity/fake/FakePlayerUtils.java
@@ -6,20 +6,23 @@ import net.minecraft.server.level.ServerPlayer;
 import java.util.List;
 
 public class FakePlayerUtils {
-    public static void createFakePlayers(List<FakePlayerInfo> fakePlayerInfos){
+    public static void createFakePlayers(List<FakePlayerInfo> fakePlayerInfos) {
         fakePlayerInfos.forEach(fakePlayerInfo -> {
-            ServerPlayer fakePlayer = EntityPlayerMPFake.createFakePlayer(fakePlayerInfo.getUsername()
-                    , fakePlayerInfo.getServer()
-                    , fakePlayerInfo.getX()
-                    , fakePlayerInfo.getY()
-                    , fakePlayerInfo.getZ()
-                    , fakePlayerInfo.getYaw()
-                    , fakePlayerInfo.getPitch()
-                    , fakePlayerInfo.getDimensionId()
-                    , fakePlayerInfo.getGamemode()
-                    , fakePlayerInfo.isIsflying());
+            ServerPlayer fakePlayer = EntityPlayerMPFake.createFakePlayer(fakePlayerInfo.getUsername(),
+                    fakePlayerInfo.getServer(),
+                    fakePlayerInfo.getX(),
+                    fakePlayerInfo.getY(),
+                    fakePlayerInfo.getZ(),
+                    fakePlayerInfo.getYaw(),
+                    fakePlayerInfo.getPitch(),
+                    fakePlayerInfo.getDimensionId(),
+                    fakePlayerInfo.getGamemode(),
+                    fakePlayerInfo.isIsflying(),
+                    fakePlayerInfo.getTextures());
             if (fakePlayer != null) {
-                WeTogether.fakePlayers.add(fakePlayer.getUUID().toString());
+                fakePlayerInfo.setUuid(fakePlayer.getUUID());
+                fakePlayerInfo.setFakePlayer(fakePlayer);
+                WeTogether.fakePlayerInfos.put(fakePlayer.getUUID(), fakePlayerInfo);
             }
         });
     }
